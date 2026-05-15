4221d4feae85fd72d68d9c22f0a1c872ae2a056c
songlindong <857344690@qq.com>
2026-05-15 10:06:38 +0800

    feat: 轮播图删除

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -14,3 +14,9 @@ type RotationRes struct {
 	// g.Meta `mime:"text/html" example:"string"`
 	RotationId int `json:"rotationId"`
 }
+
+type RotationDeleteReq struct {
+	g.Meta `path:"/backend/rotation/delete" method:"delete" tags:"内容" summary:"删除内容接口"`
+	Id     uint `v:"min:1#请选择需要删除的内容" dc:"内容id"`
+}
+type RotationDeleteRes struct{}
