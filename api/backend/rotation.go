932274103c18394cccaa5d0243abc58ee286353d
songlindong <857344690@qq.com>
2026-05-18 11:29:51 +0800

    feat: 轮播图列表

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -29,3 +29,16 @@ type RotationUpdateReq struct {
 	Sort   int    `json:"sort"         dc:"排序"`
 }
 type RotationUpdateRes struct{}
+
+type RotationGetListCommonReq struct {
+	g.Meta `path:"/backend/rotation/list" method:"get" tags:"轮播图" summary:"轮播图列表接口"`
+	Sort   int `json:"sort"   in:"query" dc:"排序类型"`
+	CommonPaginationReq
+}
+type RotationGetListCommonRes struct {
+	// g.Meta `mime:"text/html" type:"string" example:"<html/>"`
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
+}
