b1c4090738a1a55c9b356c40fef40c6cdefbc42d
songlindong <857344690@qq.com>
2026-05-19 10:46:31 +0800

    feat: 管理员相关

--- api/backend/position.go
+++ api/backend/position.go
@@ -14,7 +14,7 @@ type PositionReq struct {
 }
 type PositionRes struct {
 	// g.Meta `mime:"text/html" example:"string"`
-	PositionId int `json:"positionId"`
+	PositionId int `json:"position_id"`
 }
 
 type PositionDeleteReq struct {
--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -12,7 +12,7 @@ type RotationReq struct {
 }
 type RotationRes struct {
 	// g.Meta `mime:"text/html" example:"string"`
-	RotationId int `json:"rotationId"`
+	RotationId int `json:"rotation_id"`
 }
 
 type RotationDeleteReq struct {
